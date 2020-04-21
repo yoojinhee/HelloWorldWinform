e30ef587cd5fc8795fa80284f62e9b879e37ebff
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-04-21 14:13:12 +0900

    first commit

--- HelloWorldWinform/Form1.Designer.cs
+++ HelloWorldWinform/Form1.Designer.cs
@@ -47,7 +47,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(238, 113);
+            this.ClientSize = new System.Drawing.Size(247, 126);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
